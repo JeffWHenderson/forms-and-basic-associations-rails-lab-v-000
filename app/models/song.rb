458a92063f7461f0a38efdaafb8ea4e4879ab2c3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 22:50:34 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,35 @@ class Song < ActiveRecord::Base
   #artist_name=
   #note_contents=
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist != nil ? self.artist.name : nil
+  end
+
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
 
+  def genre_name
+    self.genre != nil ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)
+   notes.each do |content|
+     if content.strip != ''
+       self.notes.build(content: content)
+     end
+   end
+ end
+
+ def note_contents
+   content = []
+   self.notes.each do |note|
+    content << note.content
+   end
+   content
+ end
 end
