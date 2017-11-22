024ebfa08bcd82e27f46ae21a63aaa5cc8cc6da1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 22:27:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  #genre_name=
+  #artist_name=
+  #note_contents=
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 end
 
